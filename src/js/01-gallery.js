556123b3d16fcfb8b29e9fb4c62f3ad06b5cf6eb
Adaptist <adaptist1@gmail.com>
2023-04-29 01:03:31 +0300

    8.01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+// ================
+const galleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img 
+            class="gallery__image" 
+            src="${preview}" 
+            alt="${description}" 
+          />
+        </a>
+      </li>
+    `;
+  })
+  .join("");
+
+galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
+
+// ================
+var lightbox = new SimpleLightbox(".gallery__item a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
