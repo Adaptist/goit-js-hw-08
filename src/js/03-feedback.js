019bcd31b157dc17a8469dc6d9a4b01e924a0b64
Adaptist <adaptist1@gmail.com>
2023-05-02 00:02:06 +0300

    8.03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,26 @@
+const refs = {
+    formElem: document.querySelector('.feedback-form'),
+    emailInput: document.querySelector('input[name="email"]'),
+    messageInput: document.querySelector('textarea[name="message"]'),
+};
 
+refs.formElem.addEventListener('submit', e => {
+    e.preventDefault();
+});
+
+const obj = {}
+
+refs.formElem.addEventListener('input', e => {
+    const value = e.target.value;
+    const key = e.target.name;
+
+    obj [key] = value;
+    console.log(obj);
+    
+    localStorage.setItem("feedback-form-state", JSON.stringify(obj));
+});
+
+const formData = JSON.parse(localStorage.getItem("feedback-form-state")) || {};
+
+refs.emailInput.value = formData.email || '';
+refs.messageInput.value = formData.message || '';
