5a6ab20821bd9546b04a79206e294f9ba6cac63f
Adaptist <adaptist1@gmail.com>
2023-05-02 17:08:19 +0300

    8.02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,4 +2,16 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 
+let currentTime = null;
+const vimeoPlayer = document.querySelector('#vimeo-player');
+const player = new Player(vimeoPlayer);
+const THROTTLE_INTERVAL = 1000;
 
+player.on('timeupdate', throttle(reloadPlay, THROTTLE_INTERVAL));
+
+function reloadPlay(ev) { 
+    currentTime = ev.seconds;
+    localStorage.setItem('videoplayer-current-time', currentTime);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
